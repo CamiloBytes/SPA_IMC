3639cde5316c583370c1e979359a835d7030a0e2
Camilo Andres Parra Luna <camiloandres02222@gmail.com>
2025-06-16 15:53:10 -0500

    feet: the algoritmo was create

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+let myWeight
+let myHeight
+let imc
+
+myWeight=prompt(message="please, write your weight")
+myHeight=prompt(message="please, write your weight")
+
+parseInt (myWeight)
+parseInt (myHeight)
+
+imc= myWeight/(myHeight * myHeight)
+
+alert("your IMC is "+ imc)
